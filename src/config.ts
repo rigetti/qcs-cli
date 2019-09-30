028977ab91c926fc9ba5095c7c7e4426f97ddcd2
Eric Hulburd <ehulburd@rigetti.com>
2019-09-30 16:16:17 -0700

    update forest config parsing

--- src/config.ts
+++ src/config.ts
@@ -6,5 +6,43 @@ const config = ini.parse(
   fs.readFileSync(`${os.homedir()}/.qcs_config`, 'utf-8'),
 );
 
-export const userToken = config['Rigetti Forest'].user_id;
-export const publicForestServer = config['Rigetti Forest'].url;
+interface AccessToken {
+  access_token?: string;
+  refresh_token?: string;
+}
+
+export type ForestEnvironment = 'production' | 'qa' | 'staging' | 'dev' | undefined;
+
+interface ForestConfig {
+  qmiAccessToken: AccessToken;
+  userAccessToken: AccessToken;
+  userToken: string;
+  url: string;
+  forestAdminKey?: string;
+}
+
+const getAccessToken = (accessTokenType: 'qmi' | 'user', env: ForestEnvironment): AccessToken => {
+  let data: string;
+  const envTokenPath = `${os.homedir()}/.qcs/${accessTokenType}_access_token__${env}`;
+  const defaultPath = `${os.homedir()}/.qcs/qmi_access_token`;
+  if (env && fs.existsSync(envTokenPath)) {
+    data = fs.readFileSync(envTokenPath, 'utf-8');
+  } else if (fs.existsSync(defaultPath)) {
+    data = fs.readFileSync(defaultPath, 'utf-8');
+  } else {
+    return {};
+  }
+  return JSON.parse(data);
+};
+
+export const getConfig = (env?: ForestEnvironment): ForestConfig => {
+  const rawConfig = (env && config['Rigetti Forest'][env]) || config['Rigetti Forest'];
+  return {
+    url: rawConfig.url,
+    userToken: rawConfig.user_token,
+    forestAdminKey: rawConfig.forest_admin_key,
+    qmiAccessToken: getAccessToken('qmi', env),
+    userAccessToken: getAccessToken('user', env),
+  };
+};
+
