2bf5dcbc103823fbacd2c6010a3bd787db97b567
Eric Hulburd <ehulburd@rigetti.com>
2019-11-13 08:22:16 -0800

    document CommandWithCatch

--- src/command-with-catch.ts
+++ src/command-with-catch.ts
@@ -2,6 +2,14 @@ import Command from '@oclif/command';
 import { handleServerErrorIfPossible } from './http';
 import logger, { colors } from './logger';
 
+/**
+ * CommandWithCatch overrides Command#catch. If oclif throws
+ * a validation error, CommandWithCatch will pass it to the
+ * original oclif catch method. If the error is an axios
+ * response error, CommandWithCatch will log that server
+ * error for the user to read. Otherwise, it will log an
+ * unexpected error.
+ */
 export default abstract class CommandWithCatch extends Command {
 
   public logErrorAndExit(message: string) {
